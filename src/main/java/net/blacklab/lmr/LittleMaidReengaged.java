4a16061adecbb13c97b57d705acc65b9b127db6e
haoling <maya@fei-yen.jp>
2018-04-21 08:39:00 +0900

    1.12

--- src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
+++ src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
@@ -73,7 +73,7 @@ public class LittleMaidReengaged {
 	public static final String DOMAIN = "lmreengaged";
 	public static final String VERSION = "8.1.6.141";
 	public static final String ACCEPTED_MCVERSION = "[1.12.2]";
-	public static final String DEPENDENCIES = "required-after:Forge@[1.12.2-14.23.0.2517,);"
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.0.2517,);"
 			+ "required-after:net.blacklab.lib@[6.1.5.8,)";
 
 	/*
